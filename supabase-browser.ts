70250f36ef9e2a03d473db4807d0d494a9cf83e2
Richi Kothari <85846583+richikothari07@users.noreply.github.com>
2026-09-16 02:15:13 +0530

    Delete supabase-browser.ts

--- supabase-browser.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseBrowser = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || '',
-  { auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true, flowType: 'pkce' } }
-)
